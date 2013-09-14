9515c0d191427b6aab220e488113751ec7a75a19
niniyongyuan <lilinayongyuan@gmail.com>
2013-09-14 13:03:07 -0400

    add a person

---
+++ Person.py
@@ -0,0 +1,13 @@
+class Person(object):
+	def __init__(self, canvas):
+		self.size = 20
+		self.canvas = canvas
+		self.centerX = 
+		self.centerY = 0
+		self.iscollide = False
+		self.floatspeedx = 0
+		self.floatspeedy = 0
+
+	def drawPerson(self, canvas):
+		canvas.create_oval(canvas, )
+		
