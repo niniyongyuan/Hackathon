aba13fefb507381f62c3d82467cbc20c9bc38e3b
Weiqi <caiweiqi@gmail.com>
2013-09-14 13:14:43 -0400

    the basic person done

--- Person.py
+++ Person.py
@@ -1,13 +1,26 @@
 class Person(object):
 	def __init__(self, canvas):
-		self.size = 20
+		self.radius = 10
 		self.canvas = canvas
-		self.centerX = 
-		self.centerY = 0
+		#magic number need to be changed to canvas.height-size
+		self.centerX = 10
+		self.centerY = 100
 		self.iscollide = False
 		self.floatspeedx = 0
 		self.floatspeedy = 0
+		self.speed = 5
 
 	def drawPerson(self, canvas):
-		canvas.create_oval(canvas, )
-		
+		left = self.centerX - self.radius
+		right = self.centerX + self.radius
+		top = self.centerY - self.radius
+		bottom = self.centerY + self.radius
+		#change it to load the person
+		canvas.create_oval(canvas, left, right, top, bottom, fill="red")
+
+	def run(self, canvas):
+		self.centerX += self.speed
+
+
+	def jump(self, canvas):
+		self.centY += self.speed
