8ecd9ef8008635f8951894d21a5d52f6e382fde3
Megan Chen <meganche@andrew.cmu.edu>
2013-09-14 14:07:06 -0400

    Added keyPresses, as well as other init states

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
 # HackCMU 2013
 # Weiqi Cai, Megan Chen, Lina Li, Teng Zhang
 
+from Tkinter import *
+
 ###########################################
 # Game class
 ###########################################
 
 class mainGame(object):
     # Override these methods when creating your own animation
-    def mousePressed(self, event): pass
-    def keyPressed(self, event): pass
+    def keyPressed(self, event):
+        if (event.keysym == "a" or event.keysym == "Left"):
+            self.centerX -= self.speed
+            self.faceLeft = True # use this state for char dir. facing
+        if (event.keysym == "d" or event.keysym == "Right"):
+            self.centerX += self.speed
+            self.faceLeft = False
+        if (event.keysym == "w" or event.keysym == "Up"):
+            self.centerY -= self.speed
+            self.jumping = True
+        if (event.keysym == "p"):
+            self.isPaused = not self.isPaused
     def timerFired(self): pass
-    def init(self): pass
+    def init(self):
+        self.isPaused = False
+        self.jumping = False
+        self.faceLeft = False
     def redrawAll(self): pass
-    
-    # Call app.run(width,height) to get your app started
     def run(self, width=600, height=400):
         # create the root and the canvas
         root = Tk()
+        root.wm_title("Mission Gravimpossible")
         self.width = width
         self.height = height
         self.canvas = Canvas(root, width=width, height=height)
@@ -45,3 +59,7 @@ class mainGame(object):
         timerFiredWrapper()
         # and launch the app
         root.mainloop()
+
+game = mainGame()
+game.run()
+
